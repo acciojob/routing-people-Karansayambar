a511c662fecfe865816a8e1243838e0e56bcf1d3
Karan Sayambar <karansayambar@gmail.com>
2024-07-12 18:58:45 +0530

    added

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -66,8 +66,8 @@ const Users = () => {
                     <p><strong>Name:</strong> {user.name}</p>
                     <p><strong>Email:</strong> {user.email}</p>
                     <p><strong>Phone:</strong> {user.phone}</p>
-                    <p><strong>username:</strong> {user.username}</p>
-                    <p><strong>website:</strong> {user.website}</p>
+                    <p><strong>Username:</strong> {user.username}</p>
+                    <p><strong>Website:</strong> {user.website}</p>
                 </div>
             ) : (
        <p>User not Found</p>
